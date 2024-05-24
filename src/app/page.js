38920f2e0538bcd6e5e525f067d8150c9f7a62f7
biswajeetbehera123 <107670354+biswajeetbehera123@users.noreply.github.com>
2024-05-24 12:54:30 +0530

    importing excel

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@
 import { useState, useEffect, useRef } from 'react';
 import dynamic from 'next/dynamic';
 import PlayerControls from './components/PlayerControls';
+import * as XLSX from "xlsx";
 
 const DynamicMap = dynamic(() => import('./components/Map'), { ssr: false });
 
@@ -12,28 +13,48 @@ const HomePage = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [playbackSpeed, setPlaybackSpeed] = useState(1);
   const [currentTimestamp, setCurrentTimestamp] = useState(null);
+  const [data, setData] = useState([]); 
   const intervalRef = useRef(null);
   const workerRef = useRef(null);
 
+  var name = "../../public/data.xlsx"
+
+  const reader = new FileReader();
+  // reader.readAsBinaryString(e.target.files[0])
+  reader.onload = (e) => {
+    const data = e.target.result;
+    const workbook = XLSX.read(data, { type: "binary" });
+    const sheetName = workbook.SheetNames[0];
+    const sheet = workbook.Sheets[sheetName];
+    const parsedData = XLSX.utils.sheet_to_json(sheet);
+    setData(parsedData)
+  }
+
   useEffect(() => {
-    const data = [
-      { lat: 37.7749, lng: -122.4194, timestamp: 1622548800000 },
-      { lat: 37.7750, lng: -122.4184, timestamp: 1622548860000 },
-      { lat: 37.7751, lng: -122.4174, timestamp: 1622548920000 },
-    ];
+    // const inputData = [
+    //   { lat: 37.7749, lng: -122.4194, timestamp: 1622548800000 },
+    //   { lat: 37.7750, lng: -122.4184, timestamp: 1622548860000 },
+    //   { lat: 37.7751, lng: -122.4174, timestamp: 1622548920000 },
+    // ];
+
+    {data.length > 0 && (
+      inputData = [
+        { lat: 37.7749, lng: -122.4194, timestamp: 1622548800000 },
+      ]
+    )}
 
     if (window.Worker) {
       workerRef.current = new Worker(new URL('../../public/worker', import.meta.url));
       workerRef.current.onmessage = (e) => {
-        if (e.data.type === 'processedGPSData') {
-          setCoordinates(e.data.data);
-          setCurrentTimestamp(e.data.data[0].timestamp);
+        if (e.inputData.type === 'processedGPSData') {
+          setCoordinates(e.inputData.inputData);
+          setCurrentTimestamp(e.inputData.inputData[0].timestamp);
         }
       };
-      workerRef.current.postMessage({ type: 'processGPSData', data });
+      workerRef.current.postMessage({ type: 'processGPSData', inputData });
     } else {
-      setCoordinates(data);
-      setCurrentTimestamp(data[0].timestamp);
+      setCoordinates(inputData);
+      setCurrentTimestamp(inputData[0].timestamp);
     }
 
     return () => {
